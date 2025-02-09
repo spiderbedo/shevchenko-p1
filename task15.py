3c65b5f1d6b50a678e78097265d418830381a40a
spiderbedo <134661658+spiderbedo@users.noreply.github.com>
2025-02-10 00:37:30 +0700

    Update task15.py

--- task15.py
+++ task15.py
@@ -1,1 +1,9 @@
-
+cmdist = float(input())
+duym = cmdist / 2.54
+foot = duym / 12
+yard = foot / 3
+mile = yard / 1760
+print(yard, 'ярдов')
+print(mile, 'мили')
+print(foot, 'футов')
+print(duym, 'дюймов')
