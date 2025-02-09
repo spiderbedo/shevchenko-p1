3c23470c037009e391f264605847d7de32becdcd
spiderbedo <134661658+spiderbedo@users.noreply.github.com>
2025-02-10 00:10:00 +0700

    Update task10.py

--- task10.py
+++ task10.py
@@ -1,1 +1,7 @@
-
+reys = input('номер рейса: ')
+rusavia = input('название авиакомпании (на русском языке): ')
+engavia = input('название авиакомпании (на английском языке): ')
+ruscity = input('город прилета (на русском языке): ')
+engcity = input('город прилета (на английском языке): ')
+print('Заканчивается посадка на рейс', reys, rusavia, 'до', ruscity)
+print('This is the final boarding call for', engavia, 'flight', reys, 'to', engcity)
