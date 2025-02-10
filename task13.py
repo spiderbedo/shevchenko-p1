ac5ab5c09f2e22d7a459d695a2fd270fba56c6e6
spiderbedo <134661658+spiderbedo@users.noreply.github.com>
2025-02-10 14:07:25 +0700

    Update task13.py

--- task13.py
+++ task13.py
@@ -1,1 +1,5 @@
-
+import math as m
+radvsem =float(input())
+ochenrad =float(input())
+square = abs((m.pi * (radvsem ** 2)) - (m.pi * (ochenrad ** 2)))
+print('площадь покрываемой территории:', range)
